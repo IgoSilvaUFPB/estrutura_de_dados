c555b06da9c3de7ae82b0ff2cbbea62228313172
Igo Silva <igo.silva@academico.ufpb.br>
2024-02-06 13:25:47 -0300

    verifica alocação do vetor

--- matriz_dinamica/matriz_dinamica/implementacoes_matriz.cpp
+++ matriz_dinamica/matriz_dinamica/implementacoes_matriz.cpp
@@ -20,6 +20,10 @@ Matriz* cria_matriz(int linhas, int colunas) {
 		return NULL;
 	}
 	m->elementos = new float[linhas * colunas];
+	if (!m->elementos) {
+		delete m;
+		return NULL;
+	}
 	m->num_linhas = linhas;
 	m->num_colunas = colunas;
 	return m;
@@ -27,22 +31,21 @@ Matriz* cria_matriz(int linhas, int colunas) {
 
 // preenchimento da matriz
 bool preenche_matriz(Matriz* m) {
-	if (!m) {
+	if (!m || !m->elementos) {
 		return false;
 	}
 	for (int i = 0; i < m->num_linhas; i++) {
 		for (int j = 0; j < m->num_colunas; j++) {
 			cout << "Valor da linha " << i << " coluna " << j << ":";
-			cin >> m->elementos[(i * m->num_colunas) + j];
+			cin >> m->elementos[(i * m->num_colunas) + j]; // posição = (linha * número_de_colunas) + coluna
 		}
 	}
-
 	return true;
 }
 
 // exibição da matriz inteira
 void exibe_matriz(Matriz* m) {
-	if (!m) {
+	if (!m || !m->elementos) {
 		return;
 	}
 	for (int i = 0; i < m->num_linhas; i++) {
@@ -55,7 +58,7 @@ void exibe_matriz(Matriz* m) {
 
 // acesso a um elemento da matriz
 bool acessa_elemento(Matriz* m, int linha, int coluna, float* copia) {
-	if (!m || !copia) {
+	if (!m || !m->elementos || !copia) {
 		return false;
 	}
 	*copia = m->elementos[(linha * m->num_colunas) + coluna];
@@ -64,7 +67,7 @@ bool acessa_elemento(Matriz* m, int linha, int coluna, float* copia) {
 
 // mofificação de um elemento da matriz
 bool modifica_elemento(Matriz* m, int linha, int coluna, float valor) {
-	if (!m) {
+	if (!m || !m->elementos) {
 		return false;
 	}
 	m->elementos[(linha * m->num_colunas) + coluna] = valor;
@@ -73,10 +76,12 @@ bool modifica_elemento(Matriz* m, int linha, int coluna, float valor) {
 
 // liberação do espaço de memória alocado
 void destroi_matriz(Matriz** m) {
-	if (!*m) {
+	if (!*m || !(*m)->elementos) {
 		return;
 	}
-	free(*m);
+	delete[] ((*m)->elementos);
+	(*m)->elementos = NULL;
+	delete(*m);
 	*m = NULL;
 }
 
--- matriz_dinamica/matriz_dinamica/main.cpp
+++ matriz_dinamica/matriz_dinamica/main.cpp
@@ -4,17 +4,33 @@
 using namespace std;
 
 int main(void) {
+	// definição das dimensões da matriz
 	int linhas = 3, colunas = 3;
+
+	// criação da matriz
 	Matriz* matriz = cria_matriz(linhas, colunas);
+
+	// preenchimento da matriz
 	preenche_matriz(matriz);
+
+	// exibição da matriz
 	exibe_matriz(matriz);
+
 	int linha = 1, coluna = 2;
 	float copia = 0;
 	float valor = 3.14;
+
+	// alteração de um elemento da matriz
 	modifica_elemento(matriz, linha, coluna, valor);
+
+	// acesso a um elemento da matriz
 	acessa_elemento(matriz, linha, coluna, &copia);
 	cout << "Copia:" << copia << endl;
+
+	// exibição da matriz
 	exibe_matriz(matriz);
+
+	// liberação do espaço de memória alocado
 	destroi_matriz(&matriz);
 
 	return 0;
