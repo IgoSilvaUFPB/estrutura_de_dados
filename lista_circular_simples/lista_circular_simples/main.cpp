6cfba47105782a463dc418f985d5f21b3f2535d8
Igo Silva <igo.silva@academico.ufpb.br>
2024-03-15 08:11:29 -0300

    teste libera lista

--- lista_circular_simples/lista_circular_simples/main.cpp
+++ lista_circular_simples/lista_circular_simples/main.cpp
@@ -17,7 +17,16 @@ int main(void) {
 	imprime_listac(lc);
 	remove_listac(lc, 4);
 	imprime_listac(lc);
+	remove_listac(lc, 6);
+	imprime_listac(lc);
+	remove_listac(lc, 2);
+	imprime_listac(lc);
 	modifica_listac(lc, 10, 11);
 	imprime_listac(lc);
+	modifica_listac(lc, 12, 13);
+	imprime_listac(lc);
+	modifica_listac(lc, 8, 9);
+	imprime_listac(lc);
+	libera_listac(&lc);
 	return 0;
 }
