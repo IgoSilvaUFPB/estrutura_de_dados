24ece6000686570d246c5cef636b24ac1ac0dfb3
Igo Silva <igo.silva@academico.ufpb.br>
2024-03-21 20:03:39 -0300

    remocao de arquivos

--- vetor_sala_de_aula/Projeto_vetor_sala_de_aula/prototipos.h
+++
@@ -1,7 +0,0 @@
-typedef struct vetor Vetor;
-Vetor* cria_vetor(int n);
-void libera_vetor(Vetor** v);
-bool seta_vetor(Vetor* v, int pos, float valor);
-bool pega_vetor(Vetor* v, int pos, float* valor);
-void print_vetor(Vetor* v);
-bool reescalona_vetor(Vetor* v, int n);
